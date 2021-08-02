1d1c60369dbc2ee1a686881574e14741d9dccc14
quan doan <quandoan@MacBook-Pro-cua-quan.local>
2021-08-02 19:47:09 +0700

    [1.0.0] Add BabelJS.

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+let app = express();
+
+let hostname = "localhost";
+let port = 8017;
+
+app.get("/helloworld", (req, res) => {
+  res.send("<h1>Hello world !!!</h1>");
+});
+
+app.listen(port, hostname, () =>{
+  console.log(`Hello Trung Quan, I'm running at ${hostname}:${port}`);
+});
