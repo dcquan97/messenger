45fed686e688603e42b275efc841a15f2466d3e6
quan doan <quandoan@MacBook-Pro-cua-quan.local>
2021-08-08 23:30:22 +0700

    [1.0.0] Connect & create new item MongoDb.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,26 @@
 
 import express from "express";
+import ConnectDB from "./config/connectDB";
+import ContactModel from "./models/contact.model";
 
 let app = express();
+// connect to mongodb
+ConnectDB();
 
-let hostname = "localhost";
-let port = 8017;
-
-app.get("/helloworld", (req, res) => {
-  res.send("<h1>Hello world !!!</h1>");
+app.get("/test-database", async (req, res) => {
+  try {
+    let item = {
+    userId: "123456",
+    contactId: "12345776856",
+    };
+    let contact = await ContactModel.createNew(item);
+    res.send(contact);
+  }
+  catch (err) {
+    console.log(err);
+  }
 });
 
-app.listen(port, hostname, () =>{
-  console.log(`Hello Trung Quan, I'm running at ${hostname}:${port}`);
+app.listen(process.env.APP_PORT, process.env.APP_HOST, () =>{
+  console.log(`Hello Trung Quan, I'm running at ${process.env.APP_HOST}:${process.env.APP_PORT}`);
 });
