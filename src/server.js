ee530cd0bcb5ecdf5c6fca86f38fe0d037c11853
dcqbean <dtquan@dut.udn.vn>
2021-09-23 11:19:58 +0700

    edit env

--- src/server.js
+++ src/server.js
@@ -16,6 +16,12 @@ configViewEngine(app);
 //config routes
 initRoutes(app);
 
-app.listen(process.env.APP_PORT, process.env.APP_HOST, () =>{
-  console.log(`Hello Trung Quan, I'm running at ${process.env.APP_HOST}:${process.env.APP_PORT}`);
+// app.listen(process.env.APP_PORT, process.env.APP_HOST, () =>{
+//   console.log(`Hello Trung Quan, I'm running at ${process.env.APP_HOST}:${process.env.APP_PORT}`);
+// });
+var APP_HOST="localhost"
+var APP_PORT=8017
+
+app.listen(APP_PORT, APP_HOST, () =>{
+  console.log(`Hello Trung Quan, I'm running at ${APP_HOST}:${APP_PORT}`);
 });
