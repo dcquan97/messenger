6db455fb362fc7e27e5882626a65c6746479a600
dcqbean <dtquan@dut.udn.vn>
2021-10-01 11:15:14 +0700

    fix syntax

---
+++ src/config/mailer.js
@@ -0,0 +1,20 @@
+import nodeMailer from "nodemailer";
+
+let adminEmail = process.env.MAIL_USER;
+let adminPassword = process.env.MAIL_PASSWORD;
+let mailHost = process.env.MAIL_HOST;
+let mailPort = process.env.MAIL_PORT;
+
+let sendMail = (to, subject, htmlContent) => {
+  let transport = nodemailer.createTransport({
+    host: mailHost,
+    port: mailPort,
+    secure: false, // use SSL - TLS
+    auth: {
+      user: adminEmail,
+      password: adminPassword
+    }
+  });
+};
+
+module.exports = sendMail;
