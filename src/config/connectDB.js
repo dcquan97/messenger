fbfc94513df0c43feb87c1f4cb9552f9677e9ba0
dcqbean <dtquan@dut.udn.vn>
2021-11-25 16:09:06 +0700

    fix syntax deploy heroku

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -5,7 +5,7 @@ let connectDB = () => {
   mongoose.Promise = bluebird;
   // mongodb://localhost:27017/awesome_chat
   // let URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-  let URI = `${process.env.DB_URI}`
+  let URI = `${process.env.DB_URI}`;
   return mongoose.connect(URI, {useMongoClient: true});
 };
 
