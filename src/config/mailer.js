c5720cbdb525bf1a2128cc9a2d96fbbf034dc2d0
dcqbean <dtquan@dut.udn.vn>
2021-11-26 10:10:24 +0700

    fix env

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -10,7 +10,7 @@ let sendMail = (to, subject, htmlContent) => {
   let transporter = nodeMailer.createTransport({
     host: mailHost,
     port: mailPort,
-    secure: true, // use SSL - TLS
+    secure: false, // use SSL - TLS
     auth: {
       user: adminEmail,
       pass: adminPassword
